6ed9d3b69255e08b852ac30f1bd0bc254fd7ebfa
Marcos Cavalcante <marcos.bcc2011@gmail.com>
2018-04-25 17:03:09 +0100

    Add Butterknife, Glide and Timber libraries

--- app/src/main/java/projects/marcoscavalcante/com/bakinga_app/MainActivity.java
+++ app/src/main/java/projects/marcoscavalcante/com/bakinga_app/MainActivity.java
@@ -3,7 +3,8 @@ package projects.marcoscavalcante.com.bakinga_app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
